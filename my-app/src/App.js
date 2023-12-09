78c3c41ddd25e91735b39a32999dc8c4be9f7616
Sergey Kozochkin <kozochkin2000@icloud.com>
2023-12-09 23:20:45 +0500

    Work_5_Part_1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,107 +1,38 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.css';
-import { TodoInput } from './components/TodoInput';
-import { TodoList } from './components/TodoList';
-import { ref, onValue, push, remove, update } from 'firebase/database';
-import { db } from './firebase';
 
 export const App = () => {
 	const [todos, setTodos] = useState([]);
-	const [newTodo, setNewTodo] = useState('');
-	const [searchTerm, setSearchTerm] = useState('');
-	const [sortByAlphabet, setSortByAlphabet] = useState(false);
-	const [sortedTodos, setSortedTodos] = useState([]);
-	const [editableTodoId, setEditableTodoId] = useState(null);
-	const [editedTodoText, setEditedTodoText] = useState('');
-	const [originalTodos, setOriginalTodos] = useState([]);
+	const [showTodos, setShowTodos] = useState(false);
 
 	useEffect(() => {
-		const todoRef = ref(db, 'todos');
-		onValue(todoRef, (snapshot) => {
-			const data = snapshot.val();
-			if (data) {
-				const todosArray = Object.keys(data).map((key) => ({ id: key, text: data[key].text }));
-				setTodos(todosArray);
-				setOriginalTodos(todosArray);
-			} else {
-				setTodos([]);
-				setOriginalTodos([]);
-			}
-		});
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((response) => response.json())
+			.then((data) => setTodos(data.slice(0, 3)))
+			.catch((error) => console.log(error));
 	}, []);
 
-	useEffect(() => {
-		if (sortByAlphabet) {
-			const sorted = [...todos].sort((a, b) => a.text.localeCompare(b.text));
-			setSortedTodos(sorted);
-		} else {
-			setSortedTodos([...todos]);
-		}
-	}, [sortByAlphabet, todos]);
-
-	const addTodo = () => {
-		const todoRef = ref(db, 'todos');
-		push(todoRef, { text: newTodo })
-			.then((newTodoRef) => {
-				const newTodoId = newTodoRef.key;
-				setTodos([...todos, { id: newTodoId, text: newTodo }]);
-				setNewTodo('');
-			})
-			.catch((error) => console.error(error));
-	};
-
-	const handleSearch = () => {
-		const filteredTodos = originalTodos.filter((todo) => todo.text.toLowerCase().includes(searchTerm.toLowerCase()));
-		setTodos(filteredTodos);
-	};
-
-	const startEdit = (id, text) => {
-		setEditableTodoId(id);
-		setEditedTodoText(text);
-	};
-
-	const saveEdit = () => {
-		const todoRef = ref(db, `todos/${editableTodoId}`);
-		update(todoRef, { text: editedTodoText })
-			.then(() => {
-				const updatedTodos = todos.map((todo) => (todo.id === editableTodoId ? { ...todo, text: editedTodoText } : todo));
-				setTodos(updatedTodos);
-				setEditableTodoId(null);
-				setEditedTodoText('');
-			})
-			.catch((error) => console.error(error));
-	};
-
-	const deleteTodo = (id) => {
-		const todoRef = ref(db, `todos/${id}`);
-		remove(todoRef)
-			.then(() => {
-				const updatedTodos = todos.filter((todo) => todo.id !== id);
-				setTodos(updatedTodos);
-			})
-			.catch((error) => console.error(error));
+	const handleShowTodos = () => {
+		setShowTodos(true);
 	};
 
 	return (
 		<div className={styles.app}>
-			<h1 className={styles.title}>Список задач</h1>
-			<TodoInput
-				newTodo={newTodo}
-				setNewTodo={setNewTodo}
-				addTodo={addTodo}
-				setSearchTerm={setSearchTerm}
-				handleSearch={handleSearch}
-				setSortByAlphabet={() => setSortByAlphabet(!sortByAlphabet)}
-			/>
-			<TodoList
-				sortedTodos={sortedTodos}
-				editableTodoId={editableTodoId}
-				editedTodoText={editedTodoText}
-				deleteTodo={deleteTodo}
-				startEdit={startEdit}
-				saveEdit={saveEdit}
-				setEditedTodoText={setEditedTodoText}
-			/>
+			<h1 className={styles.title}>Список Задач</h1>
+			{!showTodos && (
+				<button className={styles.showButton} onClick={handleShowTodos}>
+					Показать Задачи
+				</button>
+			)}
+			{showTodos && (
+				<div className={styles['todo-list']}>
+					<ol>
+						{todos.map((todo) => (
+							<li key={todo.id}>{todo.title}</li>
+						))}
+					</ol>
+				</div>
+			)}
 		</div>
 	);
 };
